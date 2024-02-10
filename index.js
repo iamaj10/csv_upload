8607d05afc234c5426ec18bf1fe5b8cd6a5ee2f3
angaj-enfono <dheerajangaj77@gmail.com>
2024-02-10 16:14:28 +0530

    set up complete

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const express = require("express");
 const app = express();
-const port = 8000;
+const port = 8002;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const expressLayouts = require("express-ejs-layouts");
+const csv = require("csv-parser");
+const db = require("./config/mongoose");
+const bodyParser = require("body-parser");
+
+// setting layouts
+app.use(expressLayouts);
+
+// middleware for body-parser
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
+//setting up view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+// setting up routes
+app.use("/", require("./routes"));
+
+// directing the app in the given port
+app.listen(port, function (err) {
+  if (err) {
+    console.log("Error", err);
+    return;
+  }
+  console.log("Server is up and running on port: ", port);
 });
